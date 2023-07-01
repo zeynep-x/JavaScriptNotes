6a66f6c11482a1bd00cc161929f87218842b3378
zeynep-x <138033579+zeynep-x@users.noreply.github.com>
2023-07-02 00:10:25 +0300

    Introduction - Ternary Operator added

---
+++ freeCodeCamp-JavaScriptAlgorithmsAndDataStructures/Introduction/Ternary.js
@@ -0,0 +1,9 @@
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal"
+        : (a > b) ? "a is greater"
+            : "b is greater";
+}
+
+function findGreaterOrEqual(a, b) {
+    return (a === b) ? "a and b are equal" : (a > b) ? "a is greater" : "b is greater";
+}
